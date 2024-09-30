bed516f28ff390a00df5ed995cda8b5d95b136e6
DiKree <kdiana961@gmail.com>
2024-09-30 19:30:54 +0300

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,13 +1,21 @@
-function plusSlides(n, slideshowClass) {
-    var slides = document.querySelectorAll('.' + slideshowClass + ' .fade');
-    var currentSlideIndex = getCurrentSlideIndex(slideshowClass);
-    showSlides(currentSlideIndex += n, slides);
+let slideIndex = [1, 1];
+let slideId = ["mySlides", "mySlides"];
+
+// Инициализация слайдов
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
 }
 
-function getCurrentSlideIndex(slideshowClass) {
-    // Логика для получения текущего индекса слайда для каждой игры
-}
-
-function showSlides(n, slides) {
-    // Логика показа слайдов
+function showSlides(n, no) {
+    let i;
+    let slides = document.getElementsByClassName(slideId[no]);
+    if (n > slides.length) { slideIndex[no] = 1 }
+    if (n < 1) { slideIndex[no] = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex[no] - 1].style.display = "block";
 }
