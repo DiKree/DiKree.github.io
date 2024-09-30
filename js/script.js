cffafddf8ed1de5abf44a594a8b216744ed78e76
DiKree <kdiana961@gmail.com>
2024-09-30 16:53:47 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }    
-    slides[slideIndex - 1].style.display = "block";  
-    setTimeout(showSlides, 5000); // Менять слайд каждые 5 секунд
-}
-
-function plusSlides(n) {
-    slideIndex += n;
-    showSlides();
-}
-
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Страница загружена и готова к взаимодействию.");
+});
