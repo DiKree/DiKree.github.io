d3625bf32db64dda2fffb9f19657e3b01cd45a31
DiKree <kdiana961@gmail.com>
2024-09-30 17:29:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Страница загружена и готова к взаимодействию.");
-});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll(".mySlides");
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    slides.forEach(slide => slide.style.display = "none");
+    slides[slideIndex - 1].style.display = "block";
+}
