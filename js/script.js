2721428e6d67478683052916fe4d759543e6c59d
DiKree <kdiana961@gmail.com>
2024-09-30 15:20:52 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex1 = 1;
+let slideIndex2 = 1;
+showSlides(slideIndex1, 1);
+showSlides(slideIndex2, 2);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides(n, slideshowId) {
+    if (slideshowId === 1) {
+        showSlides(slideIndex1 += n, 1);
+    } else if (slideshowId === 2) {
+        showSlides(slideIndex2 += n, 2);
+    }
 }
 
-function showSlides(n) {
+function showSlides(n, slideshowId) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  // Скрыть все слайды
+    let slides;
+    
+    if (slideshowId === 1) {
+        slides = document.getElementsByClassName("mySlides1");
+        if (n > slides.length) {slideIndex1 = 1}
+        if (n < 1) {slideIndex1 = slides.length}
+    } else if (slideshowId === 2) {
+        slides = document.getElementsByClassName("mySlides2");
+        if (n > slides.length) {slideIndex2 = 1}
+        if (n < 1) {slideIndex2 = slides.length}
     }
-    slides[slideIndex - 1].style.display = "block";  // Показать текущий слайд
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Скрыть все слайды
+    }
+    
+    if (slideshowId === 1) {
+        slides[slideIndex1 - 1].style.display = "block"; // Показать текущий слайд для первого слайд-шоу
+    } else if (slideshowId === 2) {
+        slides[slideIndex2 - 1].style.display = "block"; // Показать текущий слайд для второго слайд-шоу
+    }
 }
